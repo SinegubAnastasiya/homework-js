4d2878b2c3114cdabb30fa29124d209e2d417ba4
SinegubAnastasiya <fedukevich98@mail.ru>
2024-06-26 23:02:30 +0300

    added task 54

--- task54/task54.js
+++ task54/task54.js
@@ -2,3 +2,14 @@
 // повторяющиеся символы заменены на один символ. Например, для строки "aaabbbcc"
 // результат должен быть "abc".
 
+const str = 'aaabbbcc'
+let newStr = ''
+
+const getNewStr = () => {
+    for (let i = 0; i < str.length; i++) {
+        if (!newStr.includes(str[i])) newStr += str[i]
+    }
+    return newStr
+}
+
+console.log(getNewStr());
